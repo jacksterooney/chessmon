a3d7dba8c66b3a2fd1f14aeb90675c84c1fd407c
Jack Adler <adlerljack@gmail.com>
2025-08-22 23:09:53 -0400

    Added npc scene, dialogic and interaction code@

---
+++ src/pokemon/person/Person.gd
@@ -0,0 +1,6 @@
+class_name Person
+extends CharacterBody2D
+
+#region @onready variables
+@onready var sprite := $Sprite as Sprite2D
+#endregion
---
+++ src/pokemon/person/npc/NPC.gd
@@ -0,0 +1,5 @@
+class_name NPC
+extends Person
+
+func _ready() -> void:
+	add_to_group("npcs")
